1cf5616817d366f05590cfa459aadc641e920ef1
vitalii-zavhorodnii <vince.kkr@gmail.com>
2025-06-20 16:35:39 +0200

    prepare for deploy

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Task: React.FC<Props> = ({ description, hint, image, isHintShown }) => {
   return (
     <div className="max-w-screen flex items-center flex-col">
-      <img className="h-[200px] rounded-md" src={`./src/assets/tasks/${image}`} alt="task image" />
+      <img className="h-[200px] rounded-md" src={`assets/tasks/${image}`} alt="task image" />
       <div className="mt-8 px-2 whitespace-pre-line text-xl">{description}</div>
     </div>
   );
